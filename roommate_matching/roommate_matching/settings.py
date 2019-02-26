448907452b12c4c229ed615bf3f3841d64e5929a
sahilkolwankar <sahilkolwankar@gmail.com>
2019-02-26 14:39:16 -0500

    Added React to the app

--- roommate_matching/roommate_matching/settings.py
+++ roommate_matching/roommate_matching/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'matcher'
+    'matcher',
+    'rest_framework',
+    'frontend'
 ]
 
 MIDDLEWARE = [
